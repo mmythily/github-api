187cb059a0c7a1dc8a4417b8d8178137d67be36d
mmythily <mythily.mudunuru@gmail.com>
2019-06-03 18:59:58 -0400

    added option, and github authorization

---
+++ download_avatars.js
@@ -0,0 +1,25 @@
+var request = require('request');
+var secret = require('./secrets')
+
+const getRepoContributors = (repoOwner, repoName, cb) => {
+    var option = {
+        url:`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': secret.GITHUB_TOKEN
+        }
+    }
+    request(option.url,function(error,response,body){
+        cb (error,body);
+    })
+
+}
+
+console.log('Welcome to the Github Avatar Downloader!')
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+});
+
+//aa95121d2d04f34e5303f8a7eed27df3c4298728
