ed718bfcb437be40ea89e7be21b0c680078b8f23
mmythily <mythily.mudunuru@gmail.com>
2019-06-03 23:10:41 -0400

    command line argument implemented along with a refactored function for saving images

--- download_avatars.js
+++ download_avatars.js
@@ -1,26 +1,63 @@
-var request = require('request');
-var secret = require('./secrets')
+const request = require('request');
+const secret = require('./secrets');
+const fs = require('fs');
+
+const repoOwner = process.argv[2];
+const repoName = process.argv[3];
+
+if (!repoOwner || !repoName) {
+    console.log("Please use the gitbhub username and the repo name");
+    process.exit();
+}
+
+console.log('Welcome to the Github Avatar Downloader!')
 
 const getRepoContributors = (repoOwner, repoName, cb) => {
     var option = {
         url:`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
         headers: {
             'User-Agent': 'request',
-            'Authorization': secret.GITHUB_TOKEN
+            'Authorization': 'Token ' + secret.GITHUB_TOKEN
         }
     }
-    
-    request(option.url,function(error,response,body){
-        cb (error,body);
-    })
 
+    request(option, (error, response, body) => {
+        if (response.statusCode === 200) {
+            const data = JSON.parse(body);
+            let avatarURLs = data.map( contributor => {
+                var contributorURL = {};
+                contributorURL[contributor.login] = contributor.avatar_url;
+                return contributorURL;
+            })
+            cb (error, avatarURLs);
+        }
+    });
 }
 
-console.log('Welcome to the Github Avatar Downloader!')
+const downloadImageByURL = (url, filePath) => {
+    request.get (url)
+            .on('error', err => {
+                throw err;
+            })
+            .on('response', response => {
+                console.log(`Response Status Code: ${response.statusCode}`);
+                console.log(`Response Status Message: ${response.statusMessage}`);
+                console.log(`Response Headers: ${response.headers['content-type']}`)
+            })
+            .pipe(fs.createWriteStream(filePath))
+}
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+const saveAvatarImages = (err, body) => {
     console.log("Errors:", err);
-    console.log("Result:", result);
-});
+    body.forEach( contributor => {
 
-//aa95121d2d04f34e5303f8a7eed27df3c4298728
+        let key = Object.keys(contributor);
+        const folderPath = `./avatar-images/${key[0]}.png`;
+
+        downloadImageByURL(contributor[key[0]], folderPath);
+    });
+}
+
+getRepoContributors("jquery", "jquery", saveAvatarImages);
+
+
